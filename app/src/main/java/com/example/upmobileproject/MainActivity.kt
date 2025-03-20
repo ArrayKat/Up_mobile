0383f05d0b309bd4a01484d183980133e223faef
ArrayKat <kolinichenko.05@mail.ru>
2025-03-21 00:40:23 +0300

    feat: add background color

--- app/src/main/java/com/example/upmobileproject/MainActivity.kt
+++ app/src/main/java/com/example/upmobileproject/MainActivity.kt
@@ -5,11 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.rememberNavController
 import com.example.upmobileproject.presentation.navigation.Navigation
 import com.example.upmobileproject.presentation.ui.theme.UpMobileProjectTheme
@@ -24,7 +26,7 @@ class MainActivity : ComponentActivity() {
             UpMobileProjectTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
                     Box(
-                        modifier = Modifier.fillMaxWidth()
+                        modifier = Modifier.fillMaxWidth().background(Color(0xFFF7F7F9))
                     ){
                         Navigation(controller)
                     }
